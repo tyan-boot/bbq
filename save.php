84782b28179722eeea2d240d172ea002f74bc38e
tgh980122 <tgh980122@gmail.com>
2015-01-09 21:53:43 +0800

    移除发送邮件

--- save.php
+++ save.php
@@ -30,15 +30,6 @@ $nname = stripslashes($nname);
 $nname = htmlspecialchars($nname);
 $sql -> query("insert into bbq (nick,name,contact,time,id,txt) values('$nname','$r_name','$_POST[contact]','$time','$id','$txt');");
 $sql -> close();
-include_once "mail.php";
-if (isset($_POST['email']))
-	{
-	$email = $_POST['email'];
-	if (filter_var($email, FILTER_VALIDATE_EMAIL))
-	{sendmail($email,$id);}
-	else 
-	{echo "对不起您输入的邮件地址有误，不予发送，但消息依旧会发布到首页:)";}
-	}
 echo '提交成功！';
 	echo ('<div onclick="window.location.href=\'index.php\' " style=" padding:15px;text-align:center; height:20px; width:100px; background-color:#ccc;font:微软雅黑;text-decoration: none;COLOR:#333;cursor:default;">返回查看</div>');
 }
