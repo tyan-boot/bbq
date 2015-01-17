49ee236c773a4be85db361850eec6bae5512a4af
tgh980122 <tgh980122@gmail.com>
2015-01-17 20:55:04 +0800

    宏定义邮件设置

--- bbq_config.php
+++ bbq_config.php
@@ -2,7 +2,12 @@
 
 define("USER","root",true);
 define("HOST","127.0.0.1",true);
-define("PASSWD","******",true);
+define("PASSWD","",true);
 define("DB","bbq",true);
 define("CFG","",true);
+
+define("M_Host","smtp-mail.outlook.com",true);
+define("M_Username","tyan-bbq@outlook.com",true);
+define("M_Passwd","",true);
+define("M_Port","587",true);
 
