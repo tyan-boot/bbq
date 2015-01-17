a548e9ca985c5a333a47114a77c036b2e99141f9
tgh980122 <tgh980122@gmail.com>
2015-01-17 23:43:51 +0800

    修正发送邮件方式，半自动发送

---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+
+$mail_list[0][0] = 1;$mail_list[0][1] = "735718299@qq.com";$mail_list[0][2] = "5rWL6K+V";$mail_list[0][3] = "5rWL6K+V";
+$mail_list[1][0] = 2;$mail_list[1][1] = "735718299@qq.com";$mail_list[1][2] = "5rWL6K+VMg==";$mail_list[1][3] = "5rWL6K+V";
+$mail_list[2][0] = 3;$mail_list[2][1] = "735718299@qq.com";$mail_list[2][2] = "5rWL6K+VMg==";$mail_list[2][3] = "5rWL6K+V";
+
+$i = 0;
+include_once 'bbq_config.php';
+include_once 'mail.php';
+while ($i <=2)
+{
+	sendmail($mail_list[$i][0],$mail_list[$i][1],$mail_list[$i][2],$mail_list[$i][3]);
+	$i++;
+}
