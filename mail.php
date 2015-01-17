dc4a8d21848fb8f34a9e8d503fc1ea57f4a01967
tgh980122 <tgh980122@gmail.com>
2015-01-17 19:01:08 +0800

    去除站点宏定义

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@ function sendmail($email,$id)
 	include_once 'bbq_config.php';
     $mail = new PHPMailer;
 
-    //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = M_Host;  // Specify main and backup SMTP servers
@@ -37,12 +37,12 @@ function sendmail($email,$id)
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     $mail->CharSet = "UTF-8";
     $mail ->Body = <<<body
-<p>你好：</p><p>有匿名用户在表白墙给您写了匿名消息，点击下面的链接查看</p><a href=M_Websitepid.php?id=$id>M_Websitepid.php?id=$id</a><p>如果你不能正常访问，请将上述链接复制到浏览器当中打开</p><p>请勿回复此邮件，如果你有任何疑问，请回复<a href=mailto:M_Username?subject=表白墙反馈>M_Username</a>这个邮箱，谢谢合作</p><br /><b>Tyan-Boot 敬上！</b>
+<p>你好：</p><p>有匿名用户在表白墙给您写了匿名消息，点击下面的链接查看</p><a href=http://bbq.jsahz.com/pid.php?id=$id>http://bbq.jsahz.com/pid.php?id=$id</a><p>如果你不能正常访问，请将上述链接复制到浏览器当中打开</p><p>请勿回复此邮件，如果你有任何疑问，请回复<a href=mailto:tyan-bbq@outlook.com?subject=表白墙反馈>tyan-bbq@outlook.com</a>这个邮箱，谢谢合作</p><br /><b>Tyan-Boot 敬上！</b>
 body;
     $mail ->AltBody = <<<altbody
 你好：
 有匿名用户在表白墙给您写了匿名消息，点击下面的链接查看
-M_Websitepid.php?id=$id
+http://bbq.jsahz.com/pid.php?id=$id
 如果你不能正常访问，请将上述链接复制到浏览器当中打开
 
 请勿回复此邮件，如果你有任何疑问，请回复M_Username这个邮箱，谢谢合作
@@ -53,7 +53,7 @@ altbody;
     if(!$mail->send()) {
         echo '抱歉，邮件发送失败.';
         echo '错误原因: ' . $mail->ErrorInfo;
-        echo '如果你看到此消息，请截图并发送到邮箱：<a href=mailto:M_Username?subject=表白墙反馈>M_username</a>';
+        echo '如果你看到此消息，请截图并发送到邮箱：<a href=mailto:tyan-bbq@outlook.com?subject=表白墙反馈>tyan-bbq@outlook.com</a>';
     } else {
         echo '邮件发送成功！';
     }
